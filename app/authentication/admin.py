852e613582bafe04fa219216fa4c388966e23f00
Molnár András Imre <m.andras.imre00@gmail.com>
2021-02-11 23:22:14 +0100

    Create Order datatable

--- app/authentication/admin.py
+++ app/authentication/admin.py
@@ -8,4 +8,5 @@ from .models import *
 admin.site.register(Restaurant)
 admin.site.register(Food)
 admin.site.register(Cart)
+admin.site.register(Order)
 # admin.site.register(Order)
--- app/authentication/models.py
+++ app/authentication/models.py
@@ -40,19 +40,14 @@ class Cart(models.Model):
     def __str__(self):
         return self.user_id + ' - ' + self.food.name
 
-""" 
-class Order(models.Model):
-    STATUS = (
-        ('Pending', 'Pending'),
-        ('Out of delivery', 'Out of delivery'),
-        ('Delivered', 'Delivered')
-    )
 
+class Order(models.Model):
+    user_id = models.CharField(max_length = 200, null = True)
     food = models.ForeignKey(Food, null = True, on_delete = models.SET_NULL)
-    customer = models.ForeignKey(Customer, null = True, on_delete = models.SET_NULL)
-    date_created = models.DateTimeField(auto_now_add = True, null = True)
-    status = models.CharField(max_length = 200, null = True, choices = STATUS)
+    amount = models.CharField(max_length = 200, null = True)
+    sum_price = models.CharField(max_length = 200, null = True)
+    order_date = models.DateTimeField(auto_now_add = True, null = True)
 
     def __str__(self):
-        return self.customer.name + '\'s order' 
-"""
+        return self.user_id + '\'s order ' + str(self.order_date)
+
