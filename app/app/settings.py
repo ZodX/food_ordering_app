217f96ced1ff12b5ef6dd22311579a2db854746a
Molnár András Imre <m.andras.imre00@gmail.com>
2021-04-05 16:26:39 +0200

    Enable debug

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'pf)xyyxey99uc-^t=9vftoe41jf@zhxd(tlmmh69ookd031e%h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
